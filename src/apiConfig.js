76c63e54309befb32368263958d5178416313441
Ravinder <ravinder.jollu@gmail.com>
2025-06-28 21:51:06 +0530

    push local to deploy

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,16 +1,15 @@
-export const API_BASE = process.env.REACT_APP_API_BASE
+const API_BASE = process.env.REACT_APP_API_BASE
 
 export const API_ENDPOINTS = {
-  history: `${API_BASE}/history`,
-  deleteDraft: (id) => `${API_BASE}/history/${id}`,
+  uploadFile: `${API_BASE}/upload`,
   generateDraft: `${API_BASE}/generate`,
   downloadPDF: `${API_BASE}/download`,
-  uploadFile: `${API_BASE}/upload`,
-  getDraftById: (id) => `${API_BASE}/draft/${id}`,
-  getDraftsByType: (type) => `${API_BASE}/drafts/${type}`,
   ask: `${API_BASE}/ask`,
   summarize: `${API_BASE}/summarize`,
   flaws: `${API_BASE}/flaws`,
-  getClauseSuggestions: `${API_BASE}/suggest-clauses`,
+  history: `${API_BASE}/history`,
+  getDraftById: (id) => `${API_BASE}/history/${id}`,
+  deleteDraft: (id) => `${API_BASE}/history/${id}`,
+  getClauseSuggestions: `${API_BASE}/suggest-clauses`
 }
 
